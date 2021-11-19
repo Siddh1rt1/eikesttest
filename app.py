8e4e11feaadd077fba7e651e843f5dec28c84bc7
Siddh1rt1 <93163996+Siddh1rt1@users.noreply.github.com>
2021-11-19 14:57:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 @app.route('/updatetext', methods=['POST'])
 def updatetext():
