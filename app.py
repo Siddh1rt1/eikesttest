25721f5c7042d5da881063f3061b1b051f3d260e
Siddh1rt1 <93163996+Siddh1rt1@users.noreply.github.com>
2021-11-19 15:29:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/yes', methods=['POST'])
 def updatetext():
     data= "ich bin data"
     return render_template('index.html', data=data )
